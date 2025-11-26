9eea8c407e34542dd2af20070c05785a5a8d6ee5
Ahmedbera45 <110951815+Ahmedbera45@users.noreply.github.com>
2025-11-26 15:24:39 +0300

    Refactor ProcessActionHistoryEntry properties

--- eImar/eImar.Domain/Entities/ProcessActionHistoryEntry.cs
+++ eImar/eImar.Domain/Entities/ProcessActionHistoryEntry.cs
@@ -1,15 +1,13 @@
-namespace eImar.Domain.Entities
+// ...
+public class ProcessActionHistoryEntry
 {
-    public class ProcessActionHistoryEntry
-    {
-        public int Id { get; set; }
-        public int EntryOrder { get; set; }
-        public string LogString { get; set; } = string.Empty;
+    public int Id { get; set; }
+    // ...
+    public int ProcessActionId { get; set; } // Ref -> Id
+    [ForeignKey("ProcessActionId")]
+    public ProcessAction ProcessAction { get; set; } = null!;
 
-        public int ProcessActionRef { get; set; }
-        public ProcessAction ProcessAction { get; set; } = null!;
-
-        public int ProcessApplicationRef { get; set; }
-        public ProcessApplication ProcessApplication { get; set; } = null!;
-    }
+    public int ProcessApplicationId { get; set; } // Ref -> Id
+    [ForeignKey("ProcessApplicationId")]
+    public ProcessApplication ProcessApplication { get; set; } = null!;
 }

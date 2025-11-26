62b42044ba4c0f89712337b79672b0dfce1be708
Ahmedbera45 <110951815+Ahmedbera45@users.noreply.github.com>
2025-11-26 15:21:28 +0300

    Refactor ProcessRole properties for clarity

--- eImar/eImar.Domain/Entities/ProcessRole.cs
+++ eImar/eImar.Domain/Entities/ProcessRole.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Collections.Generic;
 
 namespace eImar.Domain.Entities
@@ -7,16 +8,20 @@ namespace eImar.Domain.Entities
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
 
-        public int ProcessRef { get; set; }
+        public int ProcessId { get; set; }
+        [ForeignKey("ProcessId")]
         public Process Process { get; set; } = null!;
 
-        public int? RoleRef { get; set; }
+        public int? RoleId { get; set; }
+        [ForeignKey("RoleId")]
         public Role? Role { get; set; }
 
-        public int? MuellifGroupRef { get; set; }
+        public int? MuellifGroupId { get; set; }
+        [ForeignKey("MuellifGroupId")]
         public MuellifGroup? MuellifGroup { get; set; }
 
-        public int? MuellifTypeRef { get; set; }
+        public int? MuellifTypeId { get; set; }
+        [ForeignKey("MuellifTypeId")]
         public MuellifType? MuellifType { get; set; }
 
         public ICollection<Person> People { get; set; } = new List<Person>();
